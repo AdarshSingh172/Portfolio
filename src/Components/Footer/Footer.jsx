5f2b7a47e12dd00410808d3f0c20911a25829db3
Mithlesh kumar <110152369+AdarshSingh172@users.noreply.github.com>
2023-02-18 21:51:28 +0530

    Add files via upload

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Footer.css'
+import Wave from '../../img/wave.png';
+import Insta from '@iconscout/react-unicons/icons/uil-instagram'
+import LinkedIn from '@iconscout/react-unicons/icons/uil-linkedin'
+import Github from '@iconscout/react-unicons/icons/uil-github'
+const Footer = () => {
+  return (
+    <div className="footer">
+        <img src={Wave} alt="" style={{width: '100%'}}/>
+        <div className="f-content">
+            <span>mithleshkumar638681@gmail.com</span>
+            <div className="f-icons">
+            <a href='https://www.instagram.com/adarsh__172/'><Insta color='white' size='3rem'/></a>
+                <a href='https://www.linkedin.com/in/mithlesh-kumar-0b91bb254/'><LinkedIn color='white' size='3rem'/></a>
+                <a href='https://github.com/AdarshSingh172'><Github color='white' size='3rem'/></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
